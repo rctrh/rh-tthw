effd16e1cbd07dbb9631d44076402a51f351a7d1
rctrh <rterzi@redhat.com>
2016-02-14 21:16:35 -0500

    fixed java message

--- hello/hello.java
+++ hello/hello.java
@@ -2,8 +2,8 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, World from Java " + System.getProperty("java.version"));
+		System.out.println("Hello, Red Hat Developers World from Java " + 
+				   System.getProperty("java.version"));
 
 	}
-
 }
