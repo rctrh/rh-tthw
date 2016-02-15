4508cd06c003e021d84cac7d33a1e06a03e1ab72
rctrh <rterzi@redhat.com>
2016-02-15 16:46:30 -0500

    java rhel7 supporting files

--- hello/hello.java
+++ hello/hello.java
@@ -1,9 +1,8 @@
 public class hello {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, Red Hat Developers World from Java " + 
-				   System.getProperty("java.version"));
+    public static void main(String[] args) {
+	System.out.println("Hello, Red Hat Developers World from Java " + 
+			   System.getProperty("java.version"));
 
-	}
+    }
 }
