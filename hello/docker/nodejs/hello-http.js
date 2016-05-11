3d6f4229513018130d64f9e119cebba6ab4d8197
rctrh <rterzi@redhat.com>
2016-05-11 17:45:05 -0400

    node.js http fix listen on all interfaces (accidentally changed to localhost instead of INADDRANY when adding vars, etc.

--- hello/docker/nodejs/hello-http.js
+++ hello/docker/nodejs/hello-http.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var port = 8000;
-var laddr = '127.0.0.1';
+var laddr = '0.0.0.0';
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello, Red Hat Developers World from ' +
