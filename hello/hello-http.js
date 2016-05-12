d72c6ad9567c14b79f7e377f1d4b29ed207bf06b
rctrh <rterzi@redhat.com>
2016-05-12 16:39:01 -0400

    node hello http add missing word

--- hello/hello-http.js
+++ hello/hello-http.js
@@ -3,7 +3,7 @@ var port = 8000;
 var laddr = '0.0.0.0';
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, Red Hat Developers World from ' +
+    res.end('Hello, Red Hat Developers World from Node ' +
 	    process.version + '!\n');
     console.log('Processed request for '+ req.url);
 }).listen(port, laddr);                                                                                                                
